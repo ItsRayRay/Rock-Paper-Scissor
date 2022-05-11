f8d463ddc50e78f3edd2ff227e6dc44ac94af702
ItsRayRay <r.boerema21@gmail.com>
2022-05-11 17:07:46 +0200

    update if else statement with health bar array

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -1,44 +1,64 @@
 const playerTwoHand = ["rock","paper", "scissor"]
 const playerSelection = "rock";
+let playerIsAlive = ["♥️","♥️","♥️","♥️","♥️", false]
+let computerIsAlive = ["♥️","♥️","♥️","♥️","♥️",false]
 
 function computerPlay(min) {
-
 return min[Math.floor(Math.random() * min.length)];
+}         
 
-}
-
-            
 console.log(computerPlay(playerTwoHand)) 
 console.log(playerSelection)
 
 function playRound(playerSelection, computerSelection) {
 
 
-    if (playerSelection === computerSelection) {
-        console.log("Draw!")       
-    } else if ( playerSelection === "rock" && computerSelection ==="paper") {
-     console.log("Paper wins!")
-    
-    } else if ( playerSelection === "rock" && computerSelection ==="scissor") {
-        console.log("Rock Wins!")
-    
-       } else if ( playerSelection === "paper" && computerSelection ==="scissor") {
-        console.log("Scissor wins!")
-    
-       } else if ( playerSelection === "paper" && computerSelection ==="rock") {
-        console.log("Paper wins!")
-    
-       } else if ( playerSelection === "scissor" && computerSelection ==="paper") {                        
-        console.log("scissor wins!")
-    
-       } else if ( playerSelection === "scissor" && computerSelection ==="rock") {
-        console.log("rock wins!")
-       }
-            
-}
+    if (playerIsAlive[0] && computerIsAlive[0]) {
 
+   
+         if (playerSelection === computerSelection) {
+              console.log("Draw!")       
+          } else if ( playerSelection === "rock" && computerSelection ==="paper") {
+                console.log("Paper wins!")
+                playerIsAlive.shift("♥️")
+    
+              } else if ( playerSelection === "rock" && computerSelection ==="scissor") {
+              console.log("Rock Wins!")
+              computerIsAlive.shift("♥️")
+    
+            } else if ( playerSelection === "paper" && computerSelection ==="scissor") {
+              console.log("Scissor wins!")
+              playerIsAlive.shift("♥️")
+    
+            } else if ( playerSelection === "paper" && computerSelection ==="rock") {
+               console.log("Paper wins!")
+               computerIsAlive.shift("♥️")
+    
+             } else if ( playerSelection === "scissor" && computerSelection ==="paper") {                        
+              console.log("scissor wins!")
+              computerIsAlive.shift("♥️")
+                
+             } else if ( playerSelection === "scissor" && computerSelection ==="rock") {
+              console.log("rock wins!")
+              playerIsAlive.shift("♥️")
+            }                   
+            
+    } else if (computerIsAlive[0] === false) {
+        console.log("You Win!")
+    } else if (playerIsAlive[0] === false  ) {
+        console.log ("You Lose!")
+    }              
+
+    console.log(playerIsAlive)
+    console.log(computerIsAlive)
+
+}                                                  
+                       
 playRound(playerSelection, computerPlay(playerTwoHand))
 
+
+
+    
                                                                              
 
 
@@ -46,16 +66,5 @@ playRound(playerSelection, computerPlay(playerTwoHand))
                                                                   
 
        
-
-
-
-
-              
-
-
-
-
-
-
 
 
